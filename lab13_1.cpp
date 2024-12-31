3168a3a22237d85e63339ee65d751c005f719def
rattphumi <darkdamonig@gmail.com>
2024-12-31 16:21:13 +0700

    lab13_1_complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,37 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int N,double c[]){
+    
+    double sum, sum2, sumh = 0;
+    double sumg = 1;
+
+    for(int i =0; i<N; i++){
+        sum += a[i] ;
+        sum2 += pow(a[i],2) ;
+        sumg *= a[i] ;
+        sumh += (1/a[i]) ;
+    }
+    double xbar = sum/N ;
+    c[0] = xbar ; 
+    c[1] = sqrt((sum2/N)-pow(xbar,2)) ;
+
+    double root = 1.0/N ;
+    double GM = pow(sumg,root) ;
+    c[2] = GM ;
+
+    double HM = (N/sumh) ;
+    c[3] = HM ;
+
+    double max, min ;
+    max = a[0];
+    min = a[0];
+    for(int i=0; i<N ; i++){
+        if(a[i]>max) max = a[i] ;
+        if(a[i]<min) min = a[i] ;
+    }
+    c[4] = max ;
+    c[5] = min ;
+
+}
